5e8ff783cf80ada57b556316593059a36d63295b
jayrgee <jayrgee@fastmail.fm>
2018-05-15 22:07:40 +1000

    move babel config to .babelrc; add mssql,dotenv,babel-preset-env

--- src/db.js
+++ src/db.js
@@ -1,4 +1,20 @@
-export default callback => {
-	// connect to a database if needed, then pass it to `callback`:
-	callback();
+import sql from "mssql";
+
+const dbConfig = {
+  user: process.env.DB_USER || 'user',
+  password: process.env.DB_PASS || 'password',
+  server: process.env.DB_HOST || 'localhost',
+  database: process.env.DB_NAME || 'mssqlDb'
+};
+
+export default async callback => {
+  // connect to a database if needed, then pass it to `callback`:
+  try {
+    const pool = await sql.connect(dbConfig);
+    console.log(`connected to ${dbConfig.database} as ${dbConfig.user}`);
+    callback(sql);
+  } catch (err) {
+    // ... error checks
+    console.log(err);
+  }
 }
