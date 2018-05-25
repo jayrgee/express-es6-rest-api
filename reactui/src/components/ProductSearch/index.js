ef1f123514ac91b729de8c3a89430d57f3546a3e
jayrgee <jayrgee@fastmail.fm>
2018-05-25 18:11:21 +1000

    add product search to reactui

---
+++ reactui/src/components/ProductSearch/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+export default class ProductSearch extends React.Component {
+  state = {
+    products: []
+  };
+
+  fetchProducts() {
+    fetch("/API/products/")
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`status ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(json => {
+        this.setState({ products: json });
+      })
+      .catch(e => {
+        this.setState({
+          uiState: {
+            message: `API call failed: ${e}`,
+            fetching: false
+          }
+        });
+      });
+  }
+
+  componentDidMount() {
+    this.fetchProducts();
+  }
+
+  render() {
+    return <div>product search</div>;
+  }
+}
